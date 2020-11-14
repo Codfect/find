2d7bc1d0c96c1c0b92b90fff3fae2475c06feb31
admin <admin@pop-os.localdomain>
2020-11-14 18:16:10 -0300

    create and connect the data base

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express') //importando o express
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://Oliver:35269751@cluster0.t5lo2.mongodb.net/apifind?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(express.json())
 
